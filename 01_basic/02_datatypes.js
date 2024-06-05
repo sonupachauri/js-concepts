88593d9f61b1fa21b33ca740b41d0b885398ad95
sonupachauri <96345119+sonupachauri@users.noreply.github.com>
2024-06-05 05:00:07 +0000

    conversion data types

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert('Hello') // we are using nodejs, not browser
+
+let name ="sonu"
+let age=34
+let isLoggedIn=true
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true / false
+// null => standalone value (type is object)
+// undefined => not assign (type is undefined)
+// symbol => unique
+//object
+//array
+
+console.log(typeof null)
+
---
+++ 01_basic/03_conversionops.js
@@ -0,0 +1,25 @@
+//  let score = "33aa"
+//  let score = null
+// let score = undefined
+
+// let score = true
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+//true => 1 ; false => 0
+//null => 0
+
+// let isLoggedIn = "hitesh" // true
+// let isLoggedIn = 0 // false
+// let isLoggedIn = null // false
+let isLoggedIn = "" // false
+let booleanInLoggedIn = Boolean(isLoggedIn)
+console.log(booleanInLoggedIn);
+
+

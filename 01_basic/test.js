b8cb694e4859dc2451fea9770623f0bbbaeb984b
sonupachauri <96345119+sonupachauri@users.noreply.github.com>
2024-06-05 02:48:21 +0000

    var const let

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,18 @@
-console.log('test...')
+const accountID = 12345
+let accoutnEmail = "sonu@gmail.com"
+var accountPassword = "123456"
+accountCity = "etah"
+let accountState;
+
+// accountID=45 not allowed
+
+accoutnEmail = "hc@gmail.com"
+accountPassword="567876"
+accountCity="agra"
+
+console.log(accountID);
+
+/*
+prefer no to use var because of issue in block and functional scope
+*/
+console.table([accountID,accoutnEmail,accountPassword,accountCity,accountState]);

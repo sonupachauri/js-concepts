a4ab6623c845a9ad9596f4bf9b126603b164bb55
sonupachauri <96345119+sonupachauri@users.noreply.github.com>
2024-06-07 01:55:12 +0000

    operators

--- 01_basic/03_conversionops.js
+++ 01_basic/03_conversionops.js
@@ -18,8 +18,29 @@
 // let isLoggedIn = "hitesh" // true
 // let isLoggedIn = 0 // false
 // let isLoggedIn = null // false
-let isLoggedIn = "" // false
-let booleanInLoggedIn = Boolean(isLoggedIn)
-console.log(booleanInLoggedIn);
+// let isLoggedIn = "" // false
+// let booleanInLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanInLoggedIn);
 
+
+//operation
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+
+// console.log("1"+2)
+// console.log(2+"1")
+// console.log("2"+1+2)
+// console.log(2+1+"2")
+
+// console.log(true); // true
+// console.log(+true); // 1
+// console.log(+""); //0
+
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
 
---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,12 @@
+// console.log(2>1)
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+//  console.log("0" > true);
+
+// console.log(null == 0) // false
+// console.log(null > 0) // false
+// console.log(null >= 0) // true
+
+console.log(undefined == 0);
+console.log(undefined >= 0);

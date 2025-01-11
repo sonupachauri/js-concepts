768901de79594876377d95a9a56f6d57ba802923
sonupachauri <96345119+sonupachauri@users.noreply.github.com>
2025-01-11 17:43:26 +0000

    jsinfo added

---
+++ jsinfo/basics.js
@@ -0,0 +1,31 @@
+// let 1a; // cannot start with a digit
+
+// let my-name; // hyphens '-' aren't allowed in the name
+
+let name ="John";
+let admin = name
+// console.log(admin);
+ 
+// the "n" at the end means it's a BigInt
+const bigInt = 1234567890123456789012345678901234567890n;
+
+let message = "hello";
+message = 123456;
+
+// console.log(typeof message); number
+
+let n = 123;
+n = 12.345;
+// console.log(typeof n);
+
+// console.log(1/0); infinity
+
+// console.log('fifkjf'/5); NAN
+
+let age = null;
+
+console.log();
+
+
+
+
